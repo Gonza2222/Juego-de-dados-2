f5e256983b731a7e533c458df68941be08bc3536
Gonzalo <gonzalorivero97@gmail.com>
2024-12-28 13:05:42 -0300

    Primera subida del proyecto

---
+++ GeneralaDados/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace GeneralaDados
+{
+    partial class frmGenerala
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnJugar = new System.Windows.Forms.Button();
+            this.pctDado4 = new System.Windows.Forms.PictureBox();
+            this.pctDado3 = new System.Windows.Forms.PictureBox();
+            this.pctDado2 = new System.Windows.Forms.PictureBox();
+            this.pctDado5 = new System.Windows.Forms.PictureBox();
+            this.pctDado1 = new System.Windows.Forms.PictureBox();
+            this.btnVolver = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctDado4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctDado3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctDado2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctDado5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctDado1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.Color.Green;
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Tai Le", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.Color.Gray;
+            this.lblResultado.Location = new System.Drawing.Point(30, 287);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(809, 55);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnJugar
+            // 
+            this.btnJugar.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnJugar.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJugar.ForeColor = System.Drawing.Color.Silver;
+            this.btnJugar.Location = new System.Drawing.Point(33, 378);
+            this.btnJugar.Name = "btnJugar";
+            this.btnJugar.Size = new System.Drawing.Size(806, 46);
+            this.btnJugar.TabIndex = 4;
+            this.btnJugar.Text = "Tirar Dados";
+            this.btnJugar.UseVisualStyleBackColor = false;
+            this.btnJugar.Click += new System.EventHandler(this.btnJugar_Click);
+            // 
+            // pctDado4
+            // 
+            this.pctDado4.Image = global::GeneralaDados.Properties.Resources.dadoGirando;
+            this.pctDado4.Location = new System.Drawing.Point(531, 75);
+            this.pctDado4.Name = "pctDado4";
+            this.pctDado4.Size = new System.Drawing.Size(143, 146);
+            this.pctDado4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctDado4.TabIndex = 2;
+            this.pctDado4.TabStop = false;
+            // 
+            // pctDado3
+            // 
+            this.pctDado3.Image = global::GeneralaDados.Properties.Resources.dadoGirando;
+            this.pctDado3.Location = new System.Drawing.Point(361, 75);
+            this.pctDado3.Name = "pctDado3";
+            this.pctDado3.Size = new System.Drawing.Size(143, 146);
+            this.pctDado3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctDado3.TabIndex = 2;
+            this.pctDado3.TabStop = false;
+            // 
+            // pctDado2
+            // 
+            this.pctDado2.Image = global::GeneralaDados.Properties.Resources.dadoGirando;
+            this.pctDado2.Location = new System.Drawing.Point(197, 75);
+            this.pctDado2.Name = "pctDado2";
+            this.pctDado2.Size = new System.Drawing.Size(143, 146);
+            this.pctDado2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctDado2.TabIndex = 2;
+            this.pctDado2.TabStop = false;
+            // 
+            // pctDado5
+            // 
+            this.pctDado5.Image = global::GeneralaDados.Properties.Resources.dadoGirando;
+            this.pctDado5.Location = new System.Drawing.Point(696, 75);
+            this.pctDado5.Name = "pctDado5";
+            this.pctDado5.Size = new System.Drawing.Size(143, 146);
+            this.pctDado5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctDado5.TabIndex = 2;
+            this.pctDado5.TabStop = false;
+            // 
+            // pctDado1
+            // 
+            this.pctDado1.Image = global::GeneralaDados.Properties.Resources.dadoGirando;
+            this.pctDado1.Location = new System.Drawing.Point(30, 75);
+            this.pctDado1.Name = "pctDado1";
+            this.pctDado1.Size = new System.Drawing.Size(143, 146);
+            this.pctDado1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctDado1.TabIndex = 0;
+            this.pctDado1.TabStop = false;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnVolver.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolver.ForeColor = System.Drawing.Color.Silver;
+            this.btnVolver.Location = new System.Drawing.Point(33, 467);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(806, 46);
+            this.btnVolver.TabIndex = 5;
+            this.btnVolver.Text = "Volver a jugar";
+            this.btnVolver.UseVisualStyleBackColor = false;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // frmGenerala
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(873, 609);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.btnJugar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.pctDado4);
+            this.Controls.Add(this.pctDado3);
+            this.Controls.Add(this.pctDado2);
+            this.Controls.Add(this.pctDado5);
+            this.Controls.Add(this.pctDado1);
+            this.Name = "frmGenerala";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Jugamos al generala";
+            ((System.ComponentModel.ISupportInitialize)(this.pctDado4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctDado3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctDado2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctDado5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctDado1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctDado1;
+        private System.Windows.Forms.PictureBox pctDado5;
+        private System.Windows.Forms.PictureBox pctDado2;
+        private System.Windows.Forms.PictureBox pctDado3;
+        private System.Windows.Forms.PictureBox pctDado4;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnJugar;
+        private System.Windows.Forms.Button btnVolver;
+    }
+}
+
---
+++ GeneralaDados/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GeneralaDados.Properties; //Sirve para agregar una ruta dentro de nuestro proyecto para acortar la ruta de los picture box
+using System.Media; 
+
+namespace GeneralaDados
+{
+    public partial class frmGenerala : Form
+    {
+        public frmGenerala()
+        {
+            InitializeComponent();
+        }
+        Random R = new Random(); //Declaro el objeto generador de elementos random
+        SoundPlayer S = new SoundPlayer(); //Declaro el objeto generador de elementos con sonido
+
+        private void btnJugar_Click(object sender, EventArgs e)
+        {
+            Int32 Dado1 = R.Next(1, 7); // Declaro las variables y les asigno un valor random comprendido entre 1 y 6 
+            Int32 Dado2 = R.Next(1, 7);
+            Int32 Dado3 = R.Next(1, 7);
+            Int32 Dado4 = R.Next(1, 7);
+            Int32 Dado5 = R.Next(1, 7);
+
+            TirarDados(Dado1, pctDado1); //Procediemiento tirar dados con su variable para el numero y la pct para la imagen
+            TirarDados(Dado2, pctDado2);
+            TirarDados(Dado3, pctDado3);
+            TirarDados(Dado4, pctDado4);
+            TirarDados(Dado5, pctDado5);
+
+
+            //Preguntamos que juego se obtuvo         
+            if (Dado1 == Dado2 && Dado2 == Dado3 && Dado3 == Dado4 && Dado4 == Dado5)  //Generala son 5 dados iguales
+            {
+                lblResultado.Text = "Generala!!!";   
+                btnJugar.Enabled = false; //Desactivamos el boton comando porque ganamos el juego
+                S.SoundLocation = "Ganaste.wav";
+                S.Play();
+            }
+            else 
+            {
+                if (Dado1 == Dado2 && Dado2 == Dado3 && Dado3 == Dado4) 
+                {
+                    lblResultado.Text = "Poker";
+                    btnJugar.Enabled = false;
+                    btnVolver.Enabled = true;
+                    S.SoundLocation = "Ganaste.wav";
+                    S.Play();
+                }
+                if (Dado1 == Dado2 && Dado2 == Dado3 && Dado3 == Dado5)
+                {
+                    lblResultado.Text = "Poker";
+                    btnJugar.Enabled = false;
+                    btnVolver.Enabled = true;
+                }
+                if (Dado1 == Dado2 && Dado2 == Dado4 && Dado4 == Dado5)
+                {
+                    lblResultado.Text = "Poker";
+                    btnJugar.Enabled = false;
+                    btnVolver.Enabled = true;
+                    S.SoundLocation = "Ganaste.wav";
+                    S.Play();
+                }
+                if (Dado1 == Dado3 && Dado3 == Dado4 && Dado4 == Dado5)
+                {
+                    lblResultado.Text = "Poker";
+                    btnJugar.Enabled = false;
+                    btnVolver.Enabled = true;
+                    S.SoundLocation = "Ganaste.wav";
+                    S.Play();
+                }
+                if (Dado2 == Dado3 && Dado3 == Dado4 && Dado4 == Dado5)
+                {
+                    lblResultado.Text = "Poker";
+                    btnJugar.Enabled = false;
+                    btnVolver.Enabled = true;
+                    S.SoundLocation = "Ganaste.wav";
+                    S.Play();
+                }
+                else 
+                {
+                    Int32 SUMA = Dado1 + Dado2 + Dado3 + Dado4 + Dado5;
+                    if ( (Dado1 != Dado2 && Dado1 != Dado3 && Dado1 != Dado4 && Dado1 != Dado5 && Dado2 != Dado3 && Dado2 != Dado4 &&
+                        Dado2 != Dado5 && Dado3 != Dado4 && Dado3 != Dado5 && Dado4 != Dado5) && (SUMA == 15 || SUMA == 20 ) ) 
+                    {
+                        btnJugar.Enabled = false; 
+                        btnVolver.Enabled = true; 
+                        lblResultado.Text = "Escalera!!!";
+                        S.SoundLocation = "Ganaste.wav";
+                        S.Play();
+                    }
+                }
+            }
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            btnJugar.Enabled = true; //Volvemos a habilitar el boton "Tirar Dados"
+            btnVolver.Enabled = false; //Deshabilitamos el boton volver a jugar
+            lblResultado.Text = " ";  //Deja limpia la etiqueta que muestra el resultado
+            
+        }
+
+        private void TirarDados(Int32 N, PictureBox IMG) //Procedimiento para tirar los dados, se declara la variable "n" 
+        {                                                // y la variable IMG para las imagenes de cada dado
+            switch (N)
+            {
+                case 1:
+                    IMG.Image = GeneralaDados.Properties.Resources.dado1;
+                    break;
+                case 2:
+                    IMG.Image = GeneralaDados.Properties.Resources.dado2;
+                    break;
+                case 3:
+                    IMG.Image = GeneralaDados.Properties.Resources.dado3;
+                    break;
+                case 4:
+                    IMG.Image = GeneralaDados.Properties.Resources.dado4;
+                    break;
+                case 5:
+                    IMG.Image = GeneralaDados.Properties.Resources.dado5;
+                    break;
+                case 6:
+                    IMG.Image = GeneralaDados.Properties.Resources.dado6;
+                    break;
+            }
+        }
+    }
+}
